47736c6533afc9495b4b887c6374a6ebfc5fc634
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-09 14:11:56 -0400

    Updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,9 +9,32 @@ import PostsByUserList from "../PostsByUserList/PostsByUserList";
 
 import { getPostByTopic, getPostByUser } from "../../state/actions/posts.actions";
 
+ class App extends React.Component {
+     componentWillMount () {
+         store.dispatch(getPostByTopic(2));
+        //store.dispatch(getPostByUser(2));
+     }
+
+     render () {
+         return (
+             <div className="App">
+                 {/* <Header />
+    
+                 <Footer /> */}
+    
+                   <PostsList items={ this.props.postsByTopic.posts } />
+                  {/* <PostsByUserList items= {this.props.postsByUser.postslist} /> */}
+
+             </div>
+         );
+     }
+ };
+
+ const mapStateToProps = (state) => state;
+
 // class App extends React.Component {
-//     componentWillMount () {
-//         //store.dispatch(getPostByTopic(2));
+  //   componentWillMount () {
+    //     store.dispatch(getPostByTopic(2));
 //         store.dispatch(getPostByUser(2));
 //     }
 
@@ -23,40 +46,17 @@ import { getPostByTopic, getPostByUser } from "../../state/actions/posts.actions
 //                 <Footer /> */}
     
 //                  {/* <PostsList items={ this.props.postsByTopic.posts } />*/ } 
-//                  <PostsByUserList items= {this.props.postsByUser.postslist} /> 
+//                  <PostsByUserList items= {this.props.postslist} /> 
 
 //             </div>
 //         );
 //     }
 // };
 
-// const mapStateToProps = (state) => state;
-
-class App extends React.Component {
-    componentWillMount () {
-        //store.dispatch(getPostByTopic(2));
-        store.dispatch(getPostByUser(2));
-    }
-
-    render () {
-        return (
-            <div className="App">
-                {/* <Header />
-    
-                <Footer /> */}
-    
-                 {/* <PostsList items={ this.props.postsByTopic.posts } />*/ } 
-                 <PostsByUserList items= {this.props.postslist} /> 
-
-            </div>
-        );
-    }
-};
-
-const mapStateToProps = (state) => {
-    return {
-        postsByUser: state.postsByUser
-    }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//         postsByUser: state.postsByUser
+//     }
+// }
 
 export default connect(mapStateToProps)(App);
