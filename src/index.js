0b01c7a9d4cab06f36feb58f33e51833d3c77134
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-09 11:57:28 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { Provider } from 'react-redux';
 
 import store from './state/store';
 
-//import App from './components/App/App';
-import PostsByTopic from './components/PostsByTopic/PostsByTopic';
+import App from './components/App/App';
+//import PostsByTopic from './components/PostsByTopic/PostsByTopic';
 
 ReactDOM.render(
     <Provider store={store}>
-        <PostsByTopic />
+        <App />
     </Provider>,
 document.getElementById('root'));
