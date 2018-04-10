ecaee3760791e4ca53024b59d2d97ed83d4ec339
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-10 17:27:17 -0400

    Vote functionality and getPostsByUser

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { initialInfo } from './common.reducer';
 import { postsByTopic, postsByUser } from './posts.reducer';
+import { addVote } from './votes.reducer';
 
 export default combineReducers({
     initialInfo,
     postsByTopic,
-    postsByUser
+    postsByUser,
+    addVote
 });
