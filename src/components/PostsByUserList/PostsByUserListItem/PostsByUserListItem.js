3cf54ccd8656af00d7aa764edcc03407ab531e3d
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-09 23:19:07 -0400

    Vote functionality and getPostsByUser

--- src/components/PostsByUserList/PostsByUserListItem/PostsByUserListItem.js
+++ src/components/PostsByUserList/PostsByUserListItem/PostsByUserListItem.js
@@ -3,12 +3,13 @@ import './PostsByUserListItem.css';
 
 const PostsByUserListItem = (props) => (
     <div className="postDetails">
-        <span><h1>{props.postItem.posted_by}</h1></span>    
+        <span>{props.postItem.post_id}</span>
+        { /*<span><h1>{props.postItem.posted_by}</h1></span>    
         <span>{props.postItem.post_id}</span>
         <div className="ImageSize"><img src={props.postItem.URL}/></div>
         <span><h1>Description</h1></span>
         <span>{props.postItem.post_description}</span>
-        <div><h1>Number of votes</h1><span>{props.postItem.vote_count}</span></div>
+        <div><h1>Number of votes</h1><span>{props.postItem.vote_count}</span></div> */}
     </div>
 );
 
