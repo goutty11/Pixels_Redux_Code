1d79b281e5d62509038bad506c2221390ad40e58
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-10 09:42:52 -0400

    Vote functionality and getPostsByUser

--- src/components/PostsByUserList/PostsByUserListItem/PostsByUserListItem.js
+++ src/components/PostsByUserList/PostsByUserListItem/PostsByUserListItem.js
@@ -24,19 +24,19 @@ class PostsByUserListItem extends Component {
     }
 
     votehere = () => {
-        store.dispatch(AddVote({id: props.postItem.post_id }));
+        store.dispatch(AddVote({id: this.props.postItem.post_id }));
     }
 
     render() {
         return (
             <div className="postDetails">
-                <span><h1>{props.postItem.posted_by}</h1></span>    
-                <span>{props.postItem.post_id}</span>
-                <div className="ImageSize"><img src={props.postItem.URL}/></div>
+                <span><h1>{this.props.postItem.posted_by}</h1></span>    
+                <span>{this.props.postItem.post_id}</span>
+                <div className="ImageSize"><img src={this.props.postItem.URL}/></div>
                 <button onClick={this.votehere}>Vote here</button>
                 <span><h1>Description</h1></span>
-                <span>{props.postItem.post_description}</span>
-                <div><h1>Number of votes</h1><span>{props.postItem.vote_count}</span></div> 
+                <span>{this.props.postItem.post_description}</span>
+                <div><h1>Number of votes</h1><span>{this.props.postItem.vote_count}</span></div> 
             </div>
         );
     }
