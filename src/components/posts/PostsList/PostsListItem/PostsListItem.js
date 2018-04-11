8888e7016ac7cd5e2d507b8eb6ec63c7aabb4afe
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-11 17:12:37 -0400

    refactored getPostsByTopic functionality

--- src/components/posts/PostsList/PostsListItem/PostsListItem.js
+++ src/components/posts/PostsList/PostsListItem/PostsListItem.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './PostsListItem.css';
+import UpdateVote from '../../../../state/actions/votes.actions';
+import store from '../../../../state/store';
+
+const vote = (postid, userid, voteid ) => {
+    store.dispatch(UpdateVote(postid, userid, voteid));
+};
 
 const PostsListItem = (props) => (
     <div className="post">
@@ -8,7 +14,11 @@ const PostsListItem = (props) => (
         <div className="ImageSize"><img src={props.post.URL}/></div>
         <span><h1>Description</h1></span>
         <span>{props.post.post_description}</span>
-        <span>{props.post.vote_id ? <h1 className="voted">You voted</h1> : <button>vote now</button> }</span>
+        <span>{ props.post.vote_id 
+                    ? <h1 className="voted">You voted</h1> 
+                    : <button onClick={this.vote(props.post.post_id, 1, props.post.vote_id)}>vote now</button>
+               }
+        </span>
         <div><p>#votes</p><span>{props.post.Vote_count}</span></div>
     </div>
 );
