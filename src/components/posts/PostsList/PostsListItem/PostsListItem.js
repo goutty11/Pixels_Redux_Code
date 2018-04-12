401806df3661f5e545923b2f703243d263d414c7
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-12 18:31:19 -0400

    Added getuserdetails functionality

--- src/components/posts/PostsList/PostsListItem/PostsListItem.js
+++ src/components/posts/PostsList/PostsListItem/PostsListItem.js
@@ -34,17 +34,17 @@ class PostsListItem extends Component {
     render() {
         return (
             <div className="post">
-                <span><h1>{props.post.Posted_by}</h1></span>    
-                <span>{props.post.post_id}</span>
-                <div className="ImageSize"><img src={props.post.URL}/></div>
+                <span><h1>{this.props.post.Posted_by}</h1></span>    
+                <span>{this.props.post.post_id}</span>
+                <div className="ImageSize"><img src={this.props.post.URL}/></div>
                 <span><h1>Description</h1></span>
-                <span>{props.post.post_description}</span>
-                <span>{ props.post.vote_id 
-                            ? <h1 className="voted">You voted{props.post.vote_id}</h1> 
-                            : <button onClick={this.vote(props.post.post_id, props.post.user_id, props.post.vote_id)}>vote now</button>
+                <span>{this.props.post.post_description}</span>
+                <span>{ this.props.post.vote_id 
+                            ? <h1 className="voted">You voted{this.props.post.vote_id}</h1> 
+                            : <button onClick={this.vote(this.props.post.post_id, this.props.post.user_id, this.props.post.vote_id)}>vote now</button>
                         }
                 </span>
-                <div><p>#votes</p><span>{props.post.Vote_count}</span></div>
+                <div><p>#votes</p><span>{this.props.post.Vote_count}</span></div>
             </div>
         );
     }
