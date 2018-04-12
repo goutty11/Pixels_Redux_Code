c7bbb06bce4c3ac66f02b7bb2808440580506c1c
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-12 18:26:19 -0400

    Added getuserdetails functionality

--- src/components/posts/PostsList/PostsListItem/PostsListItem.js
+++ src/components/posts/PostsList/PostsListItem/PostsListItem.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './PostsListItem.css';
 
+import { UpdateVote } from '../../../../state/actions/votes.actions';
+import store from '../../../../state/store';
+
+const vote = (postid, userid, voteid) => {
+    store.dispatch(UpdateVote(postid, userid, voteid));
+};
+
 const PostsListItem = (props) => (
     <div className="post">
         <span><h1>{props.post.Posted_by}</h1></span>    
@@ -10,7 +17,7 @@ const PostsListItem = (props) => (
         <span>{props.post.post_description}</span>
         <span>{ props.post.vote_id 
                     ? <h1 className="voted">You voted{props.post.vote_id}</h1> 
-                    : <button>vote now</button>
+                    : <button onClick={this.vote(props.post.post_id, props.post.user_id, props.post.vote_id)}>vote now</button>
                }
         </span>
         <div><p>#votes</p><span>{props.post.Vote_count}</span></div>
