19844d851bd80908b490d87228de0861589da813
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-12 13:47:50 -0400

    Added getuserdetails functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <PostsByUser userId={this.props.initialInfo.data.currentTopic} /> 
+                <PostsByUser userId={1} /> 
                 { /* <PostsByUser userId={this.props.initialInfo.data.loggedInUserDetails.currentTopic.topic_id} /> */}
                 { /* <PostsByTopic details={this.props.initialInfo.data[0]} />  */}
                 { /* <User/> */ }
