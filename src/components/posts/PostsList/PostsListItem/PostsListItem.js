8dc360b5e58e914180b76f2212d8db99400af045
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-12 18:29:41 -0400

    Added getuserdetails functionality

--- src/components/posts/PostsList/PostsListItem/PostsListItem.js
+++ src/components/posts/PostsList/PostsListItem/PostsListItem.js
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './PostsListItem.css';
 
 import { UpdateVote } from '../../../../state/actions/votes.actions';
 import store from '../../../../state/store';
 
-const vote = (postid, userid, voteid) => {
-    store.dispatch(UpdateVote(postid, userid, voteid));
-};
 
-const PostsListItem = (props) => (
-    <div className="post">
-        <span><h1>{props.post.Posted_by}</h1></span>    
-        <span>{props.post.post_id}</span>
-        <div className="ImageSize"><img src={props.post.URL}/></div>
-        <span><h1>Description</h1></span>
-        <span>{props.post.post_description}</span>
-        <span>{ props.post.vote_id 
-                    ? <h1 className="voted">You voted{props.post.vote_id}</h1> 
-                    : <button onClick={this.vote(props.post.post_id, props.post.user_id, props.post.vote_id)}>vote now</button>
-               }
-        </span>
-        <div><p>#votes</p><span>{props.post.Vote_count}</span></div>
-    </div>
-);
+
+// const PostsListItem = (props) => (
+//     <div className="post">
+//         <span><h1>{props.post.Posted_by}</h1></span>    
+//         <span>{props.post.post_id}</span>
+//         <div className="ImageSize"><img src={props.post.URL}/></div>
+//         <span><h1>Description</h1></span>
+//         <span>{props.post.post_description}</span>
+//         <span>{ props.post.vote_id 
+//                     ? <h1 className="voted">You voted{props.post.vote_id}</h1> 
+//                     : <button onClick={this.vote(props.post.post_id, props.post.user_id, props.post.vote_id)}>vote now</button>
+//                }
+//         </span>
+//         <div><p>#votes</p><span>{props.post.Vote_count}</span></div>
+//     </div>
+// );
+
+class PostsListItem extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    vote = (postid, userid, voteid) => {
+        store.dispatch(UpdateVote(postid, userid, voteid));
+    };
+
+    render() {
+        return (
+            <div className="post">
+                <span><h1>{props.post.Posted_by}</h1></span>    
+                <span>{props.post.post_id}</span>
+                <div className="ImageSize"><img src={props.post.URL}/></div>
+                <span><h1>Description</h1></span>
+                <span>{props.post.post_description}</span>
+                <span>{ props.post.vote_id 
+                            ? <h1 className="voted">You voted{props.post.vote_id}</h1> 
+                            : <button onClick={this.vote(props.post.post_id, props.post.user_id, props.post.vote_id)}>vote now</button>
+                        }
+                </span>
+                <div><p>#votes</p><span>{props.post.Vote_count}</span></div>
+            </div>
+        );
+    }
+}
 
 
 export default PostsListItem;
