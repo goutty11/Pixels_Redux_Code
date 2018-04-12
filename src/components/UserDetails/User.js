59e5e6c3000da580c74f3e986ca1f242e1969065
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-12 11:41:22 -0400

    get user details functoinality

--- src/components/UserDetails/User.js
+++ src/components/UserDetails/User.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import store from '../../state/store';
 import { connect } from 'react-redux';
+import UserDetailsList from './userDetailsList/userDetailsList';
 
 
 class User extends Component {
     render() {
         return (
             <div>
-                {this.props.initialInfo.data.loggedInUserDetails.mail_id}
+                <UserDetailsList topicdata={this.props.initialInfo.data.loggedInUserDetails} />
             </div>
         );
     }
---
+++ src/components/userDetails/userDetailsList/userDetailsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PostsList.css';
+i
+const UserList = (props) => {
+    return (
+        <div>
+            {
+                props.topicData.mail_id
+            }
+        </div>
+    );
+};
+
+export default UserList;
