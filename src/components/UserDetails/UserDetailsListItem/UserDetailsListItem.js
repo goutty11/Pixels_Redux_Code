e3a151e84d559a16755023827559e98bf5607580
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-17 14:20:30 -0400

    Added updateUserDetails

--- src/components/UserDetails/UserDetailsListItem/UserDetailsListItem.js
+++ src/components/UserDetails/UserDetailsListItem/UserDetailsListItem.js
@@ -17,7 +17,8 @@ class UserDetailsListItem extends Component {
             userId: 1,
             firstName: this.props.userDetails.first_name,
             lastName: this.props.userDetails.last_name,
-            mailId : this.props.userDetails.mail_id
+            mailId : this.props.userDetails.mail_id,
+            postCount: this.props.userDetails.total_no_of_posts
         };
 
         this.editDetails = this.editDetails.bind(this);
@@ -51,18 +52,21 @@ class UserDetailsListItem extends Component {
         return (
             <div className="user_profile">
                 <div>
-                    <input type="text" className="first_name" value={this.state.firstName} 
+                    <input type="text" className="firstName" value={this.state.firstName} 
                         onChange={this.onChange.firstName}
                     />
                 </div>
-                <div className="last_name">
-                    <input type="text" value={this.state.lastName} 
+                <div>
+                    <input type="text" className="lastName" value={this.state.lastName} 
                         onChange={this.onChange.lastName}
                     />
                 </div>
-                <div><input type="text" value={this.state.mailId} 
+                <div><input type="text" className="mailId" value={this.state.mailId} 
                         onChange={this.onChange.mailId}
                      />
+                </div>
+                <div>
+                    {this.props.postCount}
                 </div>
                 
                 <button onClick={this.editDetails}>Save</button>
