77930c9af33a2ba48652423d524368b54f4d3544
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-17 13:28:20 -0400

    Added updateUserDetails

--- src/components/UserDetails/UserDetailsListItem/UserDetailsListItem.js
+++ src/components/UserDetails/UserDetailsListItem/UserDetailsListItem.js
@@ -7,6 +7,11 @@ class UserDetailsListItem extends Component {
     constructor(props){
         super(props);
         
+        this.onChange = {
+            firstName: this.onHandleChange.bind(this, 'firstName'),
+            lastName: this.onHandleChange.bind(this, 'lastName'),
+        }
+
         this.state = {
             userId: 1,
             firstName: this.props.userDetails.first_name,
@@ -26,18 +31,24 @@ class UserDetailsListItem extends Component {
                     ));
     };
 
-    onHandleChange = (e) => {
+    onHandleChange = (name, e) => {
         this.setState({
-            firstName: e.target.value
+            [name]: e.target.value
         });
     }
 
     render() {
         return (
             <div className="userProfile">
-                <div><input type="text" value={this.state.firstName} 
-                        onChange={(e) => {this.onHandleChange(e)}}
-                      />
+                <div>
+                    <input type="text" value={this.state.firstName} 
+                        onChange={this.onChange.firstName}
+                    />
+                </div>
+                <div>
+                    <input type="text" value={this.state.lastName} 
+                        onChange={this.onChange.lastName}
+                    />
                 </div>
                 <div>{this.state.lastName}</div>
                 <div>{this.state.mailId}</div>
