a823dd6b3be2f3a44f841a652bfccceab4caa643
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-19 13:45:30 -0400

    Updated

--- src/components/ViewBestPost/ViewBestPost.js
+++ src/components/ViewBestPost/ViewBestPost.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import store from '../../state/store';
 import { getBestPost } from '../../state/actions/posts.actions';
-import { ViewBestPostList } from './ViewBestPostList/ViewBestPostList';
+import  ViewBestPostList  from './ViewBestPostList/ViewBestPostList';
 
 class ViewBestPost extends Component {
     componentWillMount() {
