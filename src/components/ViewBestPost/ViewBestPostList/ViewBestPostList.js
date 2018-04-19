89137afe97550cd60c3370df449c735560acd897
ManojkRajasekar <manoj.k.rajasekar@gmail.com>
2018-04-19 14:11:28 -0400

    Updated

--- src/components/ViewBestPost/ViewBestPostList/ViewBestPostList.js
+++ src/components/ViewBestPost/ViewBestPostList/ViewBestPostList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 class ViewBestPostList extends Component {
+    constructor(props){
+        super(props);
+    }
     render() {
         return (
             <div>
-                {this.props.postDetails}
+                {this.props.postDetails.post_id}
             </div>
         );
     }
